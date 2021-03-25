9f3d2bd5d6fecd0559999cd86939181173b435a9
VahurK366 <78480077+VahurK366@users.noreply.github.com>
2021-03-25 18:14:02 +0200

    Add files via upload

---
+++ kujund.py
@@ -0,0 +1,13 @@
+#Iseseisev töö
+#Kujund nr.5
+#Vahur Kuuskmann - ITT20
+
+import turtle
+
+
+a = 120
+for i in range(6):    
+    turtle.right(60)       
+    for i in range(3):
+        turtle.fd(100)
+        turtle.right(a)
